cffa741e540f863185343e7571e7104a386ba47b
cuipengfei <cuipengfei2008@gmail.com>
2024-08-15 21:08:49 +0800

    add comments

--- samples/spring-boot-rest/src/main/java/org/jodconverter/sample/rest/WordFrameFilter.java
+++ samples/spring-boot-rest/src/main/java/org/jodconverter/sample/rest/WordFrameFilter.java
@@ -17,25 +17,32 @@ import org.slf4j.LoggerFactory;
 
 import static com.sun.star.uno.UnoRuntime.queryInterface;
 
+/**
+ * 一个实现Filter接口的类，用于在JOD转换过程中调整Word文档中frame的背景色
+ */
 public class WordFrameFilter implements Filter {
     private static final Logger log = LoggerFactory.getLogger(WordFrameFilter.class);
 
     @Override
     public void doFilter(OfficeContext context, XComponent document, FilterChain chain) throws Exception {
-        // Check if it is a word document
+        // 检查是否为Word文档
         XTextDocument xTextDocument = queryInterface(XTextDocument.class, document);
         if (xTextDocument == null) {
-            // Not a word document, pass it down the filter chain.
+            // 非Word文档，传递给过滤链的下一个处理
             chain.doFilter(context, document);
             return;
         }
 
+        // 调用方法用段落颜色覆盖frame颜色
         overrideFrameColorWithParagraphColor(xTextDocument);
 
-        // Continue with the filter chain
+        // 继续执行过滤链
         chain.doFilter(context, document);
     }
 
+    /**
+     * 用段落颜色覆盖frame的背景色
+     */
     public void overrideFrameColorWithParagraphColor(XTextDocument xTextDocument) throws Exception {
         // 获取文档的文本内容
         XText xText = xTextDocument.getText();
@@ -43,10 +50,11 @@ public class WordFrameFilter implements Filter {
         // 获取 XTextFramesSupplier 接口
         XTextFramesSupplier xTextFramesSupplier = queryInterface(XTextFramesSupplier.class, xTextDocument);
 
-        // 获取所有的框架
+        // 获取所有的frame
         XNameAccess xNameAccess = xTextFramesSupplier.getTextFrames();
         String[] frameNames = xNameAccess.getElementNames();
 
+        // 遍历所有frame名
         for (String frameName : frameNames) {
             try {
                 // 获取每个frame
@@ -54,12 +62,15 @@ public class WordFrameFilter implements Filter {
                 XPropertySet xProps = queryInterface(XPropertySet.class, xTextFrame);
                 Object firstParagraphBackColor = getFirstParagraphBackColor(xTextFrame.getText());
 
+                // 如果frame的背景色是负值，则用段落颜色覆盖
                 if ((int) xProps.getPropertyValue("BackColor") < 0) {
                     xProps.setPropertyValue("BackColor", firstParagraphBackColor);
                 }
+                // 如果frame的背景色RGB是负值，则用段落颜色覆盖
                 if ((int) xProps.getPropertyValue("BackColorRGB") < 0) {
                     xProps.setPropertyValue("BackColorRGB", firstParagraphBackColor);
                 }
+                // 如果frame的背景透明度为100%，则设置为0%
                 if ((Byte) xProps.getPropertyValue("BackColorTransparency") == 100) {
                     xProps.setPropertyValue("BackColorTransparency", 0);
                 }
@@ -69,11 +80,15 @@ public class WordFrameFilter implements Filter {
         }
     }
 
+    /**
+     * 获取段落的背景色
+     */
     private static int getFirstParagraphBackColor(XText xText)
-            throws NoSuchElementException, WrappedTargetException, UnknownPropertyException {
+            throws WrappedTargetException, NoSuchElementException, UnknownPropertyException {
         XEnumerationAccess xEnumerationAccess = queryInterface(XEnumerationAccess.class, xText);
         XEnumeration enumeration = xEnumerationAccess.createEnumeration();
 
+        // 遍历内容，寻找第一个段落
         while (enumeration.hasMoreElements()) {
             XTextContent xTextContent = queryInterface(XTextContent.class, enumeration.nextElement());
             if (xTextContent != null) {
@@ -83,11 +98,12 @@ public class WordFrameFilter implements Filter {
                 Object paraBackColor = xParagraphProperties.getPropertyValue("ParaBackColor");
                 log.info("this paragraph's back color is {}", paraBackColor);
 
+                // 返回找到的第一个段落的背景色
                 return (int) paraBackColor;
             }
         }
 
-        log.info("no paragraph found, will return null");
+        log.info("no paragraph found, will return 0");
         return 0;
     }
 }
