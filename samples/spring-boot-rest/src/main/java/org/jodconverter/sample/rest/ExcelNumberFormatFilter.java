2422ca9a3670d89865def6e2e3f4ccf6fadfe6a9
cuipengfei <cuipengfei2008@gmail.com>
2024-10-19 00:36:29 +0800

    smaller methods

--- samples/spring-boot-rest/src/main/java/org/jodconverter/sample/rest/ExcelNumberFormatFilter.java
+++ samples/spring-boot-rest/src/main/java/org/jodconverter/sample/rest/ExcelNumberFormatFilter.java
@@ -10,6 +10,7 @@ import com.sun.star.sheet.*;
 import com.sun.star.table.CellRangeAddress;
 import com.sun.star.table.XCell;
 import com.sun.star.uno.AnyConverter;
+import com.sun.star.util.MalformedNumberFormatException;
 import com.sun.star.util.XNumberFormats;
 import com.sun.star.util.XNumberFormatsSupplier;
 import org.jodconverter.core.office.OfficeContext;
@@ -72,34 +73,8 @@ public class ExcelNumberFormatFilter implements Filter {
                 String formatString = numberFormat.getPropertyValue("FormatString").toString();
 
                 if (formatString.equals("General")) {
-                    double value = cell.getValue();
-                    BigDecimal cellValue = BigDecimal.valueOf(value);
-                    boolean isInteger = isInteger(cellValue);
-                    int totalDigits = getTotalDigits(cellValue);
-                    int digitsBeforeDecimal = getDigitsBeforeDecimal(cellValue);
-                    int digitsAfterDecimal = getDigitsAfterDecimal(cellValue);
-
-                    if (isInteger && totalDigits >= 12) {
-                        String newFormat = "0.00000E+00";
-                        int newFormatID = xNumberFormats.queryKey(newFormat, locale, true);
-                        if (newFormatID == -1) {
-                            newFormatID = xNumberFormats.addNew(newFormat, locale);
-                        }
-                        changeNumberFormat(cellProps, newFormatID);
-                        log.info("Integer value with total digits >= 12. Changed format to {} for value={}", newFormat, cellValue);
-                    } else if (!isInteger && totalDigits >= 11) {
-                        int zerosAfterDecimal = digitsAfterDecimal - (totalDigits - 10);
-                        String newFormat = "0." + "0".repeat(Math.max(0, zerosAfterDecimal));
-                        int newFormatID = xNumberFormats.queryKey(newFormat, locale, true);
-                        if (newFormatID == -1) {
-                            newFormatID = xNumberFormats.addNew(newFormat, locale);
-                        }
-                        changeNumberFormat(cellProps, newFormatID);
-                        log.info("Decimal value with total digits >= 11. Changed format to {} for value={}", newFormat, cellValue);
-                    } else {
-                        log.info("Not going to change format for: value={}, isInteger={}, total digits={}, digitsBeforeDecimal={}, digitsAfterDecimal={}",
-                                cellValue, isInteger, totalDigits, digitsBeforeDecimal, digitsAfterDecimal);
-                    }
+                    BigDecimal cellValue = BigDecimal.valueOf(cell.getValue());
+                    handleGeneralFormat(cellProps, xNumberFormats, locale, cellValue);
                 }
             }
         } catch (Exception e) {
@@ -107,8 +82,51 @@ public class ExcelNumberFormatFilter implements Filter {
         }
     }
 
-    private void changeNumberFormat(XPropertySet cellProps, int newNumFormat)
-            throws UnknownPropertyException, PropertyVetoException, WrappedTargetException {
+    private void handleGeneralFormat(XPropertySet cellProps, XNumberFormats xNumberFormats, Locale locale, BigDecimal cellValue)
+            throws PropertyVetoException, WrappedTargetException, MalformedNumberFormatException, UnknownPropertyException {
+        boolean isInteger = isInteger(cellValue);
+        int totalDigits = getTotalDigits(cellValue);
+        int digitsBeforeDecimal = getDigitsBeforeDecimal(cellValue);
+        int digitsAfterDecimal = getDigitsAfterDecimal(cellValue);
+
+        if (isInteger && totalDigits >= 12) {
+            applyScientificNotationFormat(cellProps, xNumberFormats, locale, cellValue);
+        } else if (!isInteger && totalDigits >= 11) {
+            applyDecimalFormat(cellProps, xNumberFormats, locale, cellValue, digitsAfterDecimal);
+        } else {
+            log.info("Not going to change format for: value={}, isInteger={}, total digits={}, digitsBeforeDecimal={}, digitsAfterDecimal={}",
+                    cellValue, isInteger, totalDigits, digitsBeforeDecimal, digitsAfterDecimal);
+        }
+    }
+
+    private void applyScientificNotationFormat(XPropertySet cellProps, XNumberFormats xNumberFormats, Locale locale, BigDecimal cellValue)
+            throws PropertyVetoException, WrappedTargetException, UnknownPropertyException, MalformedNumberFormatException {
+        String newFormat = "0.00000E+00";
+        int newFormatID = addOrQueryFormat(xNumberFormats, newFormat, locale);
+        changeNumberFormat(cellProps, newFormatID);
+        log.info("Integer value with total digits >= 12. Changed format to {} for value={}", newFormat, cellValue);
+    }
+
+    private void applyDecimalFormat(XPropertySet cellProps, XNumberFormats xNumberFormats, Locale locale, BigDecimal cellValue, int digitsAfterDecimal)
+            throws MalformedNumberFormatException, PropertyVetoException, WrappedTargetException, UnknownPropertyException {
+        int zerosAfterDecimal = digitsAfterDecimal - (getTotalDigits(cellValue) - 10);
+        String newFormat = "0." + "0".repeat(Math.max(0, zerosAfterDecimal));
+        int newFormatID = addOrQueryFormat(xNumberFormats, newFormat, locale);
+        changeNumberFormat(cellProps, newFormatID);
+        log.info("Decimal value with total digits >= 11. Changed format to {} for value={}", newFormat, cellValue);
+    }
+
+    private int addOrQueryFormat(XNumberFormats xNumberFormats, String format, Locale locale)
+            throws MalformedNumberFormatException {
+        int formatID = xNumberFormats.queryKey(format, locale, false);
+        if (formatID == -1) {
+            formatID = xNumberFormats.addNew(format, locale);
+        }
+        return formatID;
+    }
+
+    private void changeNumberFormat(XPropertySet cellProps, int newNumFormat) throws
+            WrappedTargetException, UnknownPropertyException, PropertyVetoException {
         log.info("before set:{}, new id: {}", cellProps.getPropertyValue("NumberFormat"), newNumFormat);
         cellProps.setPropertyValue("NumberFormat", Integer.valueOf(newNumFormat));
         log.info("after set format id:{}", cellProps.getPropertyValue("NumberFormat"));
